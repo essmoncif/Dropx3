aacca5463853db1fc5bbbc5dccf58e1c519e16ae
essmoncif <moncefessaoudi@gmail.com>
2020-12-29 15:03:55 +0100

    add address to list of authorized

--- test/file.test.js
+++ test/file.test.js
@@ -12,4 +12,13 @@ contract("File", (accounts) => {
             assert.notEqual(address, "0x0", "Address of File contract must be not null");
         })
     })
+
+    it("Share file with someone", ()=>{
+        return File.deployed().then( (instance) => {
+            fileInstance = instance;
+            return fileInstance.share(accounts[1], {from: ownerAddress});
+        }).then( (receipt) => {
+            assert.equal(receipt.logs.length, 0, "Any error");
+        })
+    })
 })
