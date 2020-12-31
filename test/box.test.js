86562f144f0661b2c33ad3c4ce21fc635a53a7ce
essmoncif <moncefessaoudi@gmail.com>
2020-12-31 12:53:25 +0100

    test share a file with someone

--- test/box.test.js
+++ test/box.test.js
@@ -3,6 +3,7 @@ const Box = artifacts.require("Box");
 contract("Box", (accounts) => {
     var boxInstance ;
     const ownerAddress = accounts[0];
+    const myfiles = [];
 
     it("Upload file", () => {
         return Box.deployed().then((instance) => {
@@ -11,7 +12,24 @@ contract("Box", (accounts) => {
         }).then((receipt) => {
             assert.equal(receipt.logs[0].event, "Upload");
             assert.notEqual(receipt.logs[0].args.file, "0x0");
+            myfiles.push(receipt.logs[0].args.file);
             assert.equal(receipt.logs[0].args.creator, ownerAddress);
+        })
+    })
+
+    it("Share file with address", ()=> {
+        return Box.deployed().then((instance) => {
+            boxInstance = instance;
+            return boxInstance.addUserToFile(accounts[1], myfiles[0]);
+        }).then((receipt) => {
+            console.log(receipt.logs[0]);
+            assert.equal(receipt.logs[0].event, "Share");
+            assert.equal(receipt.logs[0].args.file, myfiles[0]);
+            assert.equal(receipt.logs[0].args.creator, ownerAddress);
+            assert.equal(receipt.logs[0].args.with, accounts[1]);
+            return boxInstance.addUserToFile(accounts[1], myfiles[0]);
+        }).then(assert.fail).catch((error) => {
+            assert.equal(error.reason, "ALREADY AUTHORIZED!");
         })
     })
 })
